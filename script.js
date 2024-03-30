5f11b565c560c7d02bb8731b87b71073b82f54c8
Jay <jay@mymail.com>
2024-03-30 16:24:49 -0700

    Committed

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-document.getElementById('uploadForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const file = document.getElementById('fileInput').files[0];
-    const fileName = file.name;
-    // Call your API to get the presigned URL
-    fetch('https://tzzbrzpvv1.execute-api.ap-south-1.amazonaws.com/default/presigned-url')
-        .then(response => response.json())
-        .then(data => {
-            const presignedUrl = data.url;
-            fetch(presignedUrl, {
-                method: 'PUT',
-                body: file,
-                headers: {
-                    'Content-Type': 'image/jpeg'
-                }
-            })
-            .then(uploadResponse => {
-                if (uploadResponse.ok) {
-                    console.log('Image successfully uploaded');
-                } else {
-                    console.error('Upload failed');
-                }
-            })
-            .catch(error => console.error('Error uploading image:', error));
-        })
-        .catch(error => console.error('Error getting presigned URL:', error));
+document.getElementById('uploadForm').addEventListener('submit', async function(event) {
+  event.preventDefault();
+
+  const file = document.getElementById('fileInput').files[0];
+
+  try {
+    // Get the presigned URL and filename from the API response
+    const response = await fetch('https://tzzbrzpvv1.execute-api.ap-south-1.amazonaws.com/default/presigned-url');
+    const data = await response.json();
+    const presignedUrl = data.uploadURL;  // Use uploadURL from the response
+    const photoFilename = data.photoFilename;  // Use photoFilename from the response
+
+    // Prepare the image data for upload
+    const formData = new FormData();
+    formData.append("file", file, photoFilename);  // Use photoFilename as the file name
+
+    // Upload the image using the presigned URL
+    const uploadResponse = await fetch(presignedUrl, {
+      method: 'PUT',
+      body: formData
+    });
+
+    if (uploadResponse.ok) {
+      console.log('Image successfully uploaded');
+    } else {
+      console.error('Upload failed');
+    }
+  } catch (error) {
+    console.error('Error uploading image:', error);
+  }
 });
