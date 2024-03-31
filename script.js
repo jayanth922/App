43727d30c324d60ef31c65be1979bb37190ec6f0
Jay <jay@mymail.com>
2024-03-30 21:38:13 -0700

    Committed

--- script.js
+++ script.js
@@ -16,10 +16,12 @@ document.getElementById('uploadForm').addEventListener('submit', function(event)
               body: JSON.stringify(data)
             });
 
-return response.json();
+      return response.json();
 }
 console.log(body);
     console.log(response);
     getdata("https://tzzbrzpvv1.execute-api.ap-south-1.amazonaws.com/default/presigned-url", { answer: file }).then((data) => {
   console.log(data); // JSON data parsed by `data.json()` call
-})};
+});
+
+});
