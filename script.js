6cfb328792552336bc28544ade1f0d66fdbb6cdc
Jay <jay@mymail.com>
2024-03-31 12:14:39 -0700

    Committed

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // Function to send API request to Lambda function
 async function sendRequest() {
   try {
-    // Send request to Lambda function
+    // Send request to Lambda function to get pre-signed URL
     const response = await fetch('https://tzzbrzpvv1.execute-api.ap-south-1.amazonaws.com/default/presigned-url');
     const data = await response.json();
 
     // Extract uploadURL and photoFilename from the response
     const { uploadURL, photoFilename } = data;
 
-    // Create a FormData object to hold the file and its name
-    const formData = new FormData();
-    formData.append('file', document.getElementById('fileInput').files[0], photoFilename);
+    // Get the file selected by the user
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
 
     // Send PUT request to uploadURL with the file
     const uploadResponse = await fetch(uploadURL, {
       method: 'PUT',
-      body: formData.get('file')
+      body: file
     });
     
     if (uploadResponse.ok) {
