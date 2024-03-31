60f5a1cc9c7fa2a808b58951778570cd8cfec773
Jay <jay@mymail.com>
2024-03-30 21:24:50 -0700

    Committed

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.getElementById('uploadForm').addEventListener('submit', function(event) {
-    event.preventDefault();
     const file = document.getElementById('fileInput').files[0];
     const fileName = file.name;
+    console.log(file);
+    console.log(file.type)
+
     // Call your API to get the presigned URL
     async function getdata(url = "", data = {}) {
       const response = await fetch(url, {
@@ -16,6 +18,8 @@ document.getElementById('uploadForm').addEventListener('submit', function(event)
 
 return response.json();
 }
+console.log(body);
+    console.log(response);
     getdata("https://tzzbrzpvv1.execute-api.ap-south-1.amazonaws.com/default/presigned-url", { answer: file }).then((data) => {
   console.log(data); // JSON data parsed by `data.json()` call
 });
